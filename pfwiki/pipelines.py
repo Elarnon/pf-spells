326af1c905de6d769726ea9bc1a0f54d29e91d5b
Basile Clement <basile@clement.pm>
2017-02-10 19:23:51 +0100

    Add licensing

--- pfwiki/pipelines.py
+++ pfwiki/pipelines.py
@@ -3,6 +3,24 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+'''
+    
+    Copyright (C) 2016 Basile Clement <basile-licensing@clement.pm>
+    
+    pf-spells is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    pf-spells is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 class PfwikiPipeline(object):
     def process_item(self, item, spider):
         return item
--- pfwiki/settings.py
+++ pfwiki/settings.py
@@ -6,6 +6,24 @@
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
 
+'''
+    
+    Copyright (C) 2016 Basile Clement <basile-licensing@clement.pm>
+    
+    pf-spells is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    pf-spells is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 BOT_NAME = 'pfwiki'
 
 SPIDER_MODULES = ['pfwiki.spiders']
